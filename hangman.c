5b091c951a8707fa925ddc8c729bb1edbb0778c4
Roshan Pandey <rpandox@gmail.com>
2023-07-26 21:01:50 +0545

    Added Game Elements 	Added game elements to make the game more functional

--- hangman.c
+++ hangman.c
@@ -1,4 +1,27 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_SCORE 5       // total guesses allowed + 1
+
+typedef struct {
+  char *mem;
+  size_t len;
+} Word;
+
+typedef struct {
+  Word current_word;
+  unsigned int score;
+
+  char guesses[5];       // past guesses
+  unsigned int guesses_made;
+} Game;
+
+typedef enum {
+  GAME_OVER,
+  GAME_WON,
+  GAME_RUNNING,
+} GameState;
 
 void clear_stdin() {
   int c;
@@ -9,7 +32,7 @@ void clear_stdin() {
 
 int is_alphabet(char ch) {
   return ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'));
-}
+}    
 
 char get_input() {
   char ch = 0;
